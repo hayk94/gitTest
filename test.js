80dcdca949d5bcb32b4f85782df400b420b39700
hayk94 <hayk94@hotmail.com>
2016-07-28 10:55:54 +0400

    fixing the conflicts

--- test.js
+++ test.js
@@ -2,8 +2,5 @@ alert('I alert!');
 alert('second alert!');
 alert('third alert!');
 alert('Testing github windows app');
-<<<<<<< HEAD
 alert('This alert is added after the mAF branch log');
-=======
 console.log('this log is from the mAF branch');
->>>>>>> refs/remotes/origin/myAwesomeFeature
